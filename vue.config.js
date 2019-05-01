014d8af77186c17224dbeef4bd37d55eef676a45
yinchengnuo <yinnuo96@163.com>
2019-05-01 20:34:50 +0800

    vue-config-static-dist-change

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,10 @@ module.exports = {
     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
     types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)))
   },
-  productionSourceMap: false
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fuliao-webapp-vue/'
+    : '/'
 }
 
 function addStyleResource (rule) {
